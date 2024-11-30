85f4dbfdf117744e1ddc33198b5f5753a74bfe58
wkukielk <01187243@pw.edu.pl>
2024-11-30 09:15:04 +0100

    GUI

---
+++ app/src/main/java/edu/pw/ii/pap/z29/view/GUI.java
@@ -0,0 +1,18 @@
+package edu.pw.ii.pap.z29.view;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class GUI {
+    private void createAndShowGUI() {
+        final var frame = new LoginFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void run() {
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+}
