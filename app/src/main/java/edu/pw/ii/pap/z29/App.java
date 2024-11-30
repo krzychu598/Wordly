ebdb06dabbbdbbabca70aa504b76fe908f0e02f7
wkukielk <01187243@pw.edu.pl>
2024-11-30 09:15:23 +0100

    main

--- app/src/main/java/edu/pw/ii/pap/z29/App.java
+++ app/src/main/java/edu/pw/ii/pap/z29/App.java
@@ -1,16 +1,12 @@
 package edu.pw.ii.pap.z29;
 
-import java.sql.SQLException;
+import edu.pw.ii.pap.z29.controller.MainController;
 
 
 public class App {
     public static void main(String[] args) {
         System.out.println("hello");
-        try (var database = new Database()){
-            var conn = database.getConnection();
-            System.out.println(conn.getSchema());
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        MainController controller = new MainController();
+        controller.run();
     }
 }
