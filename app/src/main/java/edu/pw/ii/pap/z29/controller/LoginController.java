99a894a1e05868d9a1e5456eac9ccc130f1acc60
wkukielk <01187243@pw.edu.pl>
2024-12-01 17:25:14 +0100

    LoginController can be asked about currentUserId

--- app/src/main/java/edu/pw/ii/pap/z29/controller/LoginController.java
+++ app/src/main/java/edu/pw/ii/pap/z29/controller/LoginController.java
@@ -1,11 +1,13 @@
 package edu.pw.ii.pap.z29.controller;
 
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
+import edu.pw.ii.pap.z29.exception.NotLoggedInException;
+
 
 public class LoginController {
     MainController mainController;
+    private Integer currentUserId = null;
 
     public LoginController(MainController mainController) {
         this.mainController = mainController;
@@ -16,20 +18,38 @@ public class LoginController {
         try {
             correct = mainController.loginPasswords.checkCredentials(
                 login, password);
+            currentUserId = 0;  // TODO: checkCredentials must return user_id;
         } catch (SQLException e) {
             mainController.sqlLogger.log(e);
         }
         if (correct) {
-            JOptionPane.showMessageDialog(
-                mainController.gui.getLoginFrame(), "Welcome!");
+            mainController.gui.disposeOfLoginFrame();
+            mainController.gui.showMainFrame();
         } else {
             JOptionPane.showMessageDialog(
                 mainController.gui.getLoginFrame(), "Try again!");
         }
     }
 
+    public int getCurrentUserId() {
+        if (currentUserId == null)
+            throw new NotLoggedInException();
+        return currentUserId;
+    }
+
     public void wantToRegister() {
         mainController.gui.disposeOfLoginFrame();
         mainController.gui.showRegisterFrame();
+    }
+
+    public void wantToLogout() {
+        mainController.gui.disposeOfMainFrame();
+        mainController.gui.showLoginFrame();
+        currentUserId = null;
+    }
+
+    public void seeProfile() {
+        mainController.gui.disposeOfMainFrame();
+        mainController.gui.showProfileFrame();
     }
 }
